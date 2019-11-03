72bf7ee9b172813274e2025f26dc1b5a650b13a5
Petr <xflajs00@stud.fit.vutbr.cz>
2019-11-03 16:17:13 +0100

    implement estimation using first method for all types of input

--- main.py
+++ main.py
@@ -8,12 +8,51 @@ from rotation import face_orientation
 from landmark_recognition import landmarks_for_face
 
 
+def method0(img, detector, predictor):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_landmarks = img.copy()
+    landmarks = landmarks_for_face(detector, predictor, gray)
+
+    if landmarks is not None and len(landmarks) != 0:
+        print('Face found.')
+        cnt = 0
+        for (x, y) in landmarks:
+            cv2.putText(img_landmarks, str(cnt), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            cnt += 1
+
+        left_eye_left_corner = landmarks[36]
+        right_eye_right_corner = landmarks[45]
+        nose = landmarks[30]
+        chin = landmarks[8]
+        left_mouth = landmarks[48]
+        right_mouth = landmarks[54]
+
+        selected_landmarks = np.array(
+            [nose, chin, left_eye_left_corner, right_eye_right_corner, left_mouth, right_mouth], dtype="double")
+
+        imgpts, modelpts, rotate_degree, nose_orienation = face_orientation(img, selected_landmarks)
+
+        cv2.line(img, nose, tuple(imgpts[1].ravel()), (0, 255, 0), 3)  # GREEN
+        cv2.line(img, nose, tuple(imgpts[0].ravel()), (255, 0,), 3)  # BLUE
+        cv2.line(img, nose, tuple(imgpts[2].ravel()), (0, 0, 255), 3)  # RED
+
+        print("Roll: " + rotate_degree[0] + "\nPitch: " + rotate_degree[1] + "\nYaw: " + rotate_degree[2])
+
+        img = cv2.resize(img, (384, 512))
+        img_landmarks = cv2.resize(img_landmarks, (384, 512))
+
+        cv2.imshow("Frame", img)
+        cv2.imshow("Frame landmarks", img_landmarks)
+    else:
+        print('Face not found.')
+
+
 parser = argparse.ArgumentParser(description='Head pose estimation (yaw, pitch, roll)')
 parser.add_argument('-i',
                     action='store',
                     dest='input_type',
-                    help='type of input: photo, video, camera',
-                    choices=['photo', 'video', 'camera'],
+                    help='type of input: image, video, camera',
+                    choices=['image', 'video', 'camera'],
                     default='camera')
 parser.add_argument('-p',
                     action='store',
@@ -36,6 +75,8 @@ if args.input_type != 'camera' and args.path is None:
 
 
 def video_estimation(method, file_path=None):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
     if file_path is None:
         cap = cv2.VideoCapture(0)
     else:
@@ -44,7 +85,7 @@ def video_estimation(method, file_path=None):
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
-            cv2.imshow('test', frame)
+            method0(frame, detector, predictor)
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
         else:
@@ -52,50 +93,19 @@ def video_estimation(method, file_path=None):
     cap.release()
 
 
+def pose_estimation(method, file_path):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
+    img = cv2.imread(file_path)
+    if method == 0:
+        method0(img, detector, predictor)
+    cv2.waitKey()
+
+
 if args.input_type == 'camera':
     video_estimation(args.method)
 elif args.input_type == 'video':
     video_estimation(args.method, args.path)
+elif args.input_type == 'image':
+    pose_estimation(args.method, args.path)
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
-
-img = cv2.imread(args.path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_landmarks = img.copy()
-landmarks = landmarks_for_face(detector, predictor, gray)
-
-if landmarks is not None:
-    print('Face found.')
-    cnt = 0
-    for (x, y) in landmarks:
-        cv2.putText(img_landmarks, str(cnt), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        cnt += 1
-
-    left_eye_left_corner = landmarks[36]
-    right_eye_right_corner = landmarks[45]
-    nose = landmarks[30]
-    chin = landmarks[8]
-    left_mouth = landmarks[48]
-    right_mouth = landmarks[54]
-
-    selected_landmarks = np.array(
-        [landmarks[30], landmarks[8], landmarks[36], landmarks[45], landmarks[48], landmarks[54]], dtype="double")
-
-    imgpts, modelpts, rotate_degree, nose_orienation = face_orientation(img, selected_landmarks)
-
-    cv2.line(img, nose, tuple(imgpts[1].ravel()), (0, 255, 0), 3)  # GREEN
-    cv2.line(img, nose, tuple(imgpts[0].ravel()), (255, 0,), 3)  # BLUE
-    cv2.line(img, nose, tuple(imgpts[2].ravel()), (0, 0, 255), 3)  # RED
-
-    print("Roll: " + rotate_degree[0] + "\nPitch: " + rotate_degree[1] + "\nYaw: " + rotate_degree[2])
-
-    img = cv2.resize(img, (384, 512))
-    img_landmarks = cv2.resize(img_landmarks, (384,512))
-
-    cv2.imshow("Frame", img)
-    cv2.imshow("Frame landmarks", img_landmarks)
-    cv2.imwrite('data/tmp.png', img)
-    cv2.waitKey()
-else:
-    print('Face not found.')
--- rotation.py
+++ rotation.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 24 11:58:14 2018
-@author: jerry
-"""
-
 import cv2
 import math
 import numpy as np
@@ -33,7 +27,7 @@ def face_orientation(frame, image_points):
 
     dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
     (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix,
-                                                                  dist_coeffs, flags=cv2.cv2.SOLVEPNP_ITERATIVE)
+                                                                  dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 
     axis = np.float32([[500, 0, 0],
                        [0, 500, 0],
@@ -53,39 +47,3 @@ def face_orientation(frame, image_points):
     yaw = math.degrees(math.asin(math.sin(yaw)))
 
     return imgpts, modelpts, (str(int(roll)), str(int(pitch)), str(int(yaw))), (image_points[4], image_points[5])
-
-
-"""
-for line in iter(f):
-    img_info = line.split(' ')
-    img_path = img_info[0]
-    frame = cv2.imread(img_path)
-    landmarks = map(int, img_info[1:])
-
-    print
-    img_path
-    imgpts, modelpts, rotate_degree, nose = face_orientation(frame, landmarks)
-
-    cv2.line(frame, nose, tuple(imgpts[1].ravel()), (0, 255, 0), 3)  # GREEN
-    cv2.line(frame, nose, tuple(imgpts[0].ravel()), (255, 0,), 3)  # BLUE
-    cv2.line(frame, nose, tuple(imgpts[2].ravel()), (0, 0, 255), 3)  # RED
-
-    remapping = [2, 3, 0, 4, 5, 1]
-    for index in range(len(landmarks) / 2):
-        random_color = tuple(np.random.random_integers(0, 255, size=3))
-
-        cv2.circle(frame, (landmarks[index * 2], landmarks[index * 2 + 1]), 5, random_color, -1)
-        cv2.circle(frame, tuple(modelpts[remapping[index]].ravel().astype(int)), 2, random_color, -1)
-
-    #    cv2.putText(frame, rotate_degree[0]+' '+rotate_degree[1]+' '+rotate_degree[2], (10, 30),
-    #                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
-    #                thickness=2, lineType=2)
-
-    for j in xrange(len(rotate_degree)):
-        cv2.putText(frame, ('{:05.2f}').format(float(rotate_degree[j])), (10, 30 + (50 * j)), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 255, 0), thickness=2, lineType=2)
-
-    cv2.imwrite('test2/' + img_path.split('/')[1], frame)
-
-f.close()
-"""
