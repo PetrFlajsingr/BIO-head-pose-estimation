3f65f88510c8dddcb3a2adae23fd8aa8e0b78d72
PetrFlajsingr <flajsingr@gmail.com>
2019-11-03 13:45:28 +0100

    add test data

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ import dlib
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
-img = cv2.imread('/home/petr/Desktop/bio/1.jpg')
+img = cv2.imread('data/6.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = detector(gray)
 for face in faces:
-    x1 = face.left()
-    y1 = face.top()
-    x2 = face.right()
-    y2 = face.bottom()
-    # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-
     landmarks = predictor(gray, face)
 
     for n in range(0, 68):
@@ -24,4 +18,5 @@ for face in faces:
         cv2.circle(img, (x, y), 4, (255, 0, 0), -1)
 
 cv2.imshow("Frame", img)
+cv2.waitKey()
 
