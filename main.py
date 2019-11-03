b989601bedef35806971f1677df043644d4f8201
Petr <xflajs00@stud.fit.vutbr.cz>
2019-11-03 15:57:59 +0100

    add argparser

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import argparse
+
 import cv2
 import numpy as np
 import dlib
 from landmark_recognition import landmarks_for_face
+
+
+parser = argparse.ArgumentParser(description='Head pose estimation (yaw, pitch, roll)')
+parser.add_argument('-i',
+                    action='store',
+                    dest='input_type',
+                    help='type of input: photo, video, camera',
+                    choices=['photo', 'video', 'camera'],
+                    default='camera')
+parser.add_argument('-p',
+                    action='store',
+                    dest='path',
+                    help='path to input file',
+                    required=False,
+                    type=str)
+parser.add_argument('-m',
+                    action='store',
+                    dest='method',
+                    help='method used to estimate head pose: 0 - 3D model, 1 - planes, 2 - estimated 000 location',
+                    choices=[0, 1, 2],
+                    type=int,
+                    default=0)
+args = parser.parse_args()
+
+if args.input_type != 'camera' and args.path is None:
+    print('You need to provide file input.')
+    exit(0)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
