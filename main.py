47f2ea4fa3dc9af9aafb11b3707468f211ae5b62
PetrFlajsingr <flajsingr@gmail.com>
2019-11-03 13:49:09 +0100

    add rotate function to avoid not finding landmarks

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import numpy as np
 import dlib
 
 
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
-img = cv2.imread('data/6.jpg')
+img = cv2.imread('data/4.jpg')
+img = rotate_image(img, 40)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = detector(gray)
 for face in faces:
