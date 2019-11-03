c3669d7f419ba320fc4c42d71f849756feae45ae
PetrFlajsingr <flajsingr@gmail.com>
2019-11-03 13:34:23 +0100

    impl for landmark detection

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+img = cv2.imread('~/Desktop/bio/1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = detector(gray)
+for face in faces:
+    x1 = face.left()
+    y1 = face.top()
+    x2 = face.right()
+    y2 = face.bottom()
+    # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    landmarks = predictor(gray, face)
+
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        cv2.circle(img, (x, y), 4, (255, 0, 0), -1)
+
+cv2.imshow("Frame", img)
+cv2.waitKey()
