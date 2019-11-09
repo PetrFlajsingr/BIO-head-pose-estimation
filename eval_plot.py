7a97949c4e78705bc1df132cc7e928f1d36c7ab5
Petr <xflajs00@stud.fit.vutbr.cz>
2019-11-09 12:44:11 +0100

    add plotting function

---
+++ eval_plot.py
@@ -0,0 +1,12 @@
+import plotly.graph_objects as go
+import numpy as np
+
+
+def create_plot(correct_data, detected_data):
+    assert(len(correct_data) == len(detected_data))
+    x_axis = np.linspace(0, 1, len(correct_data))
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x_axis, y=correct_data), mode='lines', name='Expected')
+    fig.add_trace(go.Scatter(x=x_axis, y=detected_data), mode='lines', name='Expected')
+
+    fig.show()
